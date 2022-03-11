2c635d78459e7f424277955e6dbb39e6583b1c75
marcelog5 <marcelovictorg2@gmail.com>
2022-03-11 18:48:19 -0300

    Added initial functions

---
+++ front/js/script.js
@@ -0,0 +1,52 @@
+var wordNumber = 0;
+var letterNumber = 0;
+
+function letterHandle(letter) {
+  if (letterNumber < 5) {
+    const word = document.getElementById("word" + wordNumber);
+    const letterNode = word.children[letterNumber];
+    letterNode.append(letter);
+
+    letterNumber++;
+  }
+}
+
+function deleteHandle() {
+  if (letterNumber > 0) {
+    const word = document.getElementById("word" + wordNumber);
+    const letterNode = word.children[letterNumber - 1];
+    letterNode.innerHTML = "";
+    letterNumber--;
+  }
+}
+
+function submitHandle() {
+  if (letterNumber != 5) {
+    return;
+  }
+
+  if (wordNumber < 6) {
+    letterNumber = 0;
+    wordNumber++;
+  } else {
+    console.log("enviar");
+  }
+
+  function wrongLetter(idLetter, idWord) {
+    const word = document.getElementById("word" + idWord);
+    const letterNode = word.children[idLetter];
+    letterNode.style.backgroundColor = "red";
+  }
+
+  function RightLetter(idLetter, idWord) {
+    const word = document.getElementById("word" + idWord);
+    const letterNode = word.children[idLetter];
+    letterNode.style.backgroundColor = "yellow";
+  }
+
+  function RightLetterPosition(idLetter, idWord) {
+    const word = document.getElementById("word" + idWord);
+    const letterNode = word.children[idLetter];
+    letterNode.style.backgroundColor = "green";
+  }
+}
