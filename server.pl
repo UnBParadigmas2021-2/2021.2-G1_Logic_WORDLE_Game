9c1cfa29b552cd9b60932f4986be0de486c4d5ba
Matheus <88738347+J-Matheus@users.noreply.github.com>
2022-03-13 02:33:35 -0300

    Add verify endpoint

--- server.pl
+++ server.pl
@@ -3,20 +3,31 @@
 :- use_module(library(http/http_json)).
 
 % URL handlers.
-:- http_handler('/', handle_request, []).
+:- http_handler('/', handle_request, [methods([post])]).
 :- http_handler('/start', handle_start_request, [methods([get])]).
 :- http_handler('/verify', handle_verify_request, []).
 
-verify(Word) :-
-    selected_word(Word).
+verify(_{guess:Guess}, _{status:Status,data:Data}) :-
+    selected_word(Word),
+    consult('wordle'),
+    atom_string(Atom_Guess, Guess),
+    wordle(Atom_Guess, Word, Status, Colors),
+    guesses(Guesses),
+    append(Guesses, [_{guess:Guess,colors:Colors}], NewGuesses),
+    retractall(guesses(_)),
+    assertz(guesses(NewGuesses)),
+    guesses(Data).
 
 handle_verify_request(Request) :-
-    verify(Word),
-    reply_json_dict(Word).
+    http_read_json_dict(Request, Query),
+    verify(Query, Answer),
+    reply_json_dict(Answer).
 
 start(Word) :-
     consult('word_utils'),
     get_random_word(Selected_Word),
+    retractall(guesses(_)),
+    assertz(guesses([])),
     retractall(selected_word(_)),
     assertz(selected_word(Selected_Word)),
     selected_word(Word).
--- wordle.pl
+++ wordle.pl
@@ -1,5 +1,3 @@
-:- include(words).
-
 % check if a char in on a list
 check_char(_, []):- fail, !.
 check_char(X, [H|_]):-
@@ -11,7 +9,7 @@ check_char(X, [_|T]):-
 get_colors([], [], _, _, COLOR, COLOR).
 % add a entry green on the list 
 get_colors([H1 | T1], [H2 | T2], GUESS, WORD, COLORS, [green | Z]):-
-    write(H1), nl,
+    % write(H1), nl,
     H1=H2,
     get_colors(T1, T2, GUESS, WORD, COLORS, Z).
 % add a entry yello on the list
@@ -21,10 +19,19 @@ get_colors([H1 | T1], [_ | T2], GUESS, WORD, COLORS, [yellow | Z]):-
 get_colors([_ | T1], [_ | T2], GUESS, WORD, COLORS, [cyan | Z]):-
     get_colors(T1, T2, GUESS, WORD, COLORS, Z).
 
-wordle(GUESS, WORD):-
-    word5(GUESS),
-    atom_chars(GUESS, L_GUESS),
-    atom_chars(WORD, L_WORD),
-    get_colors(L_GUESS, L_WORD, L_GUESS, L_WORD, [], X),
-    write(X), nl, !.
+define_success_status(TIPS, STATUS) :-
+    member(cyan, TIPS) -> atom_string(STATUS, "going"); atom_string(STATUS, "ok"),
+    member(yellow, TIPS) -> atom_string(STATUS, "going"); atom_string(STATUS, "ok").
+
+wordle(GUESS, WORD, STATUS, TIPS) :-
+    consult('words'),
+    word5(GUESS) ->
+        atom_chars(GUESS, L_GUESS),
+    	atom_chars(WORD, L_WORD),
+	get_colors(L_GUESS, L_WORD, L_GUESS, L_WORD, [], TIPS),
+        define_success_status(TIPS, STATUS), !    	
+	% write(TIPS), nl, !.
+	;
+	atom_string(STATUS, "fail"),
+	TIPS is 0.
 
